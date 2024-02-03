9ad09c3cdf87a6f5eac5427fcce151c70fd1523b
lenawuu <lenawu24@gmail.com>
2024-02-03 11:46:00 +0000

    Co-authored-by: technologeve <technologeve@users.noreply.github.com>

--- virtual/main.py
+++ virtual/main.py
@@ -1,37 +1,118 @@
+""" First script training the chatbots"""
+
+# External imports
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
-class Character:
-    def __init__(self, name, conversation, targetWords):
+class ChatbotLevel:
+    # Train the chatbot
+    def __init__(self, name, conversation, targetWords, corpusModules):
         self.name = name
         self.conversation = conversation
         self.targetWords = targetWords
         
         self.chatbot = ChatBot(name)
+        trainer = ChatterBotCorpusTrainer(self.chatbot)
+        for module in corpusModules:
+            trainer.train(module)
+        listTrainer = ListTrainer(self.chatbot)
+        listTrainer.train(conversation)
+
+
+
+first_level = ChatbotLevel(
+    "Hopper", 
+    [
+        "Bonjour",
+        "Salut",
+        "Bonsoir",        
+        "Ca va?",
+        "Comment allez-vous?",
+        "Bien, merci",
+        "Super!"
+    ], 
+    ["bonjour", "salut", "bonsoir", "coucou"], 
+    ["chatterbot.corpus.french.greetings"])
+
+
+# second_level = ChatbotLevel(
+#     "Hopper", 
+#     [
+#         "Bonjour",
+#         "Salut",
+#         "Bonsoir",        
+#         "Ca va?",
+#         "Comment allez-vous?",
+#         "Bien, merci",
+#         "Super!"
+#         "Où est la tour Eiffel?",
+#         "à gauche",
+#         "à droit",
+#         "Ici",
+#         "Là-bas",
+#         "Je ne sais pas",
+#         "Merci",
+#     ], 
+#     ["bonjour", "ça va"], 
+#     ["chatterbot.corpus.french.greetings", "chatterbot.corpus.french.conversations"])
+
+# third_level = ChatbotLevel(
+#     "Hopper", 
+#     [
+#         "Bonjour",
+#         "Salut",
+#         "Bonsoir",        
+#         "Ca va?",
+#         "Comment allez-vous?",
+#         "Bien, merci",
+#         "Super!"
+#         "Où est la tour Eiffel?",
+#         "à gauche",
+#         "à droit",
+#         "Ici",
+#         "Là-bas",
+#         "Je ne sais pas",
+#         "Merci",
+#     ], 
+#     ["bonjour"], 
+#     ["chatterbot.corpus.french.greetings", "chatterbot.corpus.french.conversations", "chatterbot.corpus.french.food"])
+
+
+def conversation_loop(level):
+    
+    # Record the score for this level
+    this_level_user_score = 0
+
+    while True:
+        # Ask the user to input
+        user_prompt = input("You: ")
+        # print(userScore)
+        print(this_level_user_score)
         
+        # Add to the user's score if they use key phrases from the lessom
+        for word in level.targetWords:
+            if word in user_prompt.lower():
+                this_level_user_score += 1
+                
+        # Stop the conversation 
+        if "au revoir" in user_prompt.lower():
+            print(level.name + ": Au revoir!")
+            print("Score:", this_level_user_score)
+            break
 
-chatbot = ChatBot("Le Hopper")
+        # Get a response from the chatbot
+        response = level.chatbot.get_response(user_prompt)
 
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("chatterbot.corpus.french")
+        # Shot the chatbot response
+        print(level.name + ": ", response)
 
-userScore = 0
-targetWords = ["bonjour"]
+    return this_level_user_score
 
-conversation = [
-    "Bonjour",
-    "salut",
-    "bonsoir",
-    "ça va?",
-    "comment allez-vous?"
-]
-
-while True:
-    print("Le Hopper: Bonjour!")
-    user_prompt = input("You: ")
-    response = chatbot.get_response(user_prompt)
-    print("Le Hopper: ", response)
-
-    if "au revoir" in user_prompt.lower():
-        print("Le Hopper: Au revoir!")
-        break
+# Start the first level
+# Prompt for the user to start the conversation
+print("Say hello to Hopper!")
+print("When you are finished with the conversation, say \"Au Revior\", which means goodbye!") 
+# TODO: Run the example conversation
+# TODO: Fix the score update
+# Initialise the chatbot   
+additional_score = conversation_loop(first_level)
